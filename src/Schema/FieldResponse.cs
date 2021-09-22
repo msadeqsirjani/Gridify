cb2c37e1d6cddbb47b3ef9362037d4e111c1f40d
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2021-09-22 21:44:25 +0330

    Create FieldResponse.cs

---
+++ src/Schema/FieldResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Fop.Meta;
+
+namespace Fop.Schema
+{
+    public class FieldResponse
+    {
+        public FieldResponse FillFieldResponse(List<IMeta> metas)
+        {
+            if (metas == null)
+                return this;
+
+            metas.ForEach(x =>
+            {
+
+            });
+        }
+    }
+}
