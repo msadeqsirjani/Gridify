7fdc38e21f6bae3f30b464ddf1d9fc60d6f0bcb4
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2021-09-22 21:44:38 +0330

    Create SchemaResponse.cs

---
+++ src/Schema/SchemaResponse.cs
@@ -0,0 +1,30 @@
+﻿using Fop.Meta;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fop.Schema
+{
+    public class SchemaResponse
+    {
+        public IEnumerable<FieldResponse> Fields { get; set; }
+        public IEnumerable<FilterResponse> Filters { get; set; }
+        public IEnumerable<OrderResponse> Orders { get; set; }
+
+        public SchemaResponse()
+        {
+
+        }
+
+        public SchemaResponse(IReadOnlyCollection<KeyValuePair<string, IMeta>> pairs)
+        {
+            pairs?.GroupBy(x => x.Key)
+                .ToList()
+                .ForEach(g =>
+                {
+                    var fieldMetaKeys = pairs.Where(x => x.Key.Equals(g.Key))
+                        .Select(x => x.Value)
+                        .ToList();
+                });
+        }
+    }
+}
