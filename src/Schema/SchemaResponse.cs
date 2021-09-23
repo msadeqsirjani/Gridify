2ca9f18b94ae998e1bd081ff5f476374c77800f0
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2021-09-23 11:54:15 +0330

    Update SchemaResponse.cs

--- src/Schema/SchemaResponse.cs
+++ src/Schema/SchemaResponse.cs
@@ -6,13 +6,15 @@ namespace Fop.Schema
 {
     public class SchemaResponse
     {
-        public IEnumerable<FieldResponse> Fields { get; set; }
-        public IEnumerable<FilterResponse> Filters { get; set; }
-        public IEnumerable<OrderResponse> Orders { get; set; }
+        public List<FieldResponse> Fields { get; set; }
+        public List<FilterResponse> Filters { get; set; }
+        public List<OrderResponse> Orders { get; set; }
 
         public SchemaResponse()
         {
-
+            Fields = new List<FieldResponse>();
+            Filters = new List<FilterResponse>();
+            Orders = new List<OrderResponse>();
         }
 
         public SchemaResponse(IReadOnlyCollection<KeyValuePair<string, IMeta>> pairs)
@@ -24,7 +26,43 @@ namespace Fop.Schema
                     var fieldMetaKeys = pairs.Where(x => x.Key.Equals(g.Key))
                         .Select(x => x.Value)
                         .ToList();
+
+                    Fields.Add(new FieldResponse().FillFieldResponse(fieldMetaKeys));
+
+                    fieldMetaKeys.Where(m => m.GetType() == typeof(MetaFilter)).ToList().ForEach(meta =>
+                    {
+                        var metaFilter = (MetaFilter)meta;
+                        Filters.Add(new FilterResponse
+                        {
+                            Key = metaFilter.Key,
+                            Value = metaFilter.Value,
+                            Assembly = metaFilter.Assembly,
+                            DataType = metaFilter.DataType,
+                            Operator = metaFilter.Operator,
+                            Fullname = metaFilter.Fullname
+                        });
+                    });
+
+                    fieldMetaKeys.Where(m => m.GetType() == typeof(MetaOrder)).ToList().ForEach(f =>
+                    {
+                        var metaFilter = (MetaOrder)f;
+                        Orders.Add(new OrderResponse
+                        {
+                            OrderBy = metaFilter.OrderBy,
+                            Direction = metaFilter.Direction
+                        });
+                    });
                 });
+
+            if (Fields.Any(f => f.Sequence > 0))
+                return;
+
+            var sequence = 0;
+            Fields.ToList().ForEach(f =>
+            {
+                f.Sequence = sequence;
+                sequence++;
+            });
         }
     }
 }
