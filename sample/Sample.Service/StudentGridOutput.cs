3fc8a749545c86de231cfc2cf150bff493fc39cc
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-09-29 22:42:35 +0330

    Create StudentGridOutput.cs

---
+++ sample/Sample.Service/StudentGridOutput.cs
@@ -0,0 +1,22 @@
+﻿using Gridify.Meta;
+using Gridify.Result;
+using Sample.Entity;
+
+namespace Sample.Repository
+{
+    public class StudentGridResponse : GridResponse<Student>
+    {
+        public override void InitSchema()
+        {
+            Meta(x => x.Id)
+                .IsKey()
+                .IsVisible(false);
+
+            Meta(x => x.Name)
+                .AddTitle("عنوان");
+
+            Meta(x => x.Birthday)
+                .AddTitle("تاریخ تولد");
+        }
+    }
+}
