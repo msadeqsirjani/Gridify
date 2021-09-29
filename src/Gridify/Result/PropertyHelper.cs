8460ff54380bf9b85ca2358699c81e7bd2f239a3
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-09-29 22:43:09 +0330

    Create PropertyHelper.cs

---
+++ src/Gridify/Result/PropertyHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Gridify.Result
+{
+    public static class PropertyHelper<T>
+    {
+        public static PropertyInfo GetProperty<TValue>(
+            Expression<Func<T, TValue>> selector)
+        {
+            Expression body = selector;
+
+            if (body is LambdaExpression lambdaExpression) body = lambdaExpression.Body;
+
+            return body.NodeType switch
+            {
+                ExpressionType.MemberAccess => (PropertyInfo)((MemberExpression)body).Member,
+                _ => throw new InvalidOperationException()
+            };
+        }
+    }
+}
