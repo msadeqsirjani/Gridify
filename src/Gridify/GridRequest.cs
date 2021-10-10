5364c218e6dda925ab93f76ba9829d160f6f98fe
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2021-10-10 13:25:34 +0330

    upgrade to 1.1.2

--- src/Gridify/GridRequest.cs
+++ src/Gridify/GridRequest.cs
@@ -15,16 +15,8 @@ namespace Gridify
         public bool Schema { get; set; }
     }
 
-    public class GridRequest<TSource> where TSource : new()
+    public class GridRequest<TSource> : GridRequest
     {
-        public IEnumerable<FilterList> Filters { get; set; }
-
-        public IEnumerable<Order.Order> Orders { get; set; }
-
-        public Pagination Pagination { get; set; }
-
-        public bool Schema { get; set; }
-
         public TSource Parameter { get; set; }
     }
 }
